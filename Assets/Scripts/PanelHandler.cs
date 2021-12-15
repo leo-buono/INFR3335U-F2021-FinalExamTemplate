c203e9bc1ebb959e8918887dd9ebe63ae24b1297
leo-buono <56273373+leo-buono@users.noreply.github.com>
2021-12-15 13:12:16 -0500

    based

--- Assets/Scripts/PanelHandler.cs
+++ Assets/Scripts/PanelHandler.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
-public class PanelHandler : MonoBehaviour
+public class PanelHandler : MonoBehaviourPunCallbacks
 {
+    
+    public InputField createInput;
+    public InputField joinInput;
+    // public Text Error;
+
+    // string errorInfo = "Error!";
+
+    // Start is called before the first frame update
+    public override void OnJoinedRoom()
+    {
+        //PhotonNetwork.LoadLevel("SampleScene");
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        //Error.text = message;
+        Debug.Log("Failed to Create room!");
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        //Error.text = message;
+        Debug.Log(message);
+    }
     private void Start() 
     {
         LobbyPanel.SetActive(false);
         MainMenuPanel.SetActive(true);
+        PhotonNetwork.ConnectUsingSettings();
     }
     public GameObject LobbyPanel;
     public GameObject MainMenuPanel;
@@ -29,12 +53,18 @@ public class PanelHandler : MonoBehaviour
     public void OnLeaveLobbyClick()
     {
         //Run the loading scene
-        SceneManager.LoadScene("Lobby");
+        SceneManager.LoadScene("Loading");
     }
 
     public void OnCreateGameClick()
     {
         //Load game
-        SceneManager.LoadScene("Arena");
+        PhotonNetwork.LoadLevel("Arena");
+        //PhotonNetwork.JoinRoom(joinInput.text);
+        if (string.IsNullOrEmpty(createInput.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(createInput.text);
     }
 }
